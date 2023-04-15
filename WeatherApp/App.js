e7371a4737cedf8cf3b14179b4285e904239a4f8
Le-Fox <smkupa@gmail.com>
2023-04-15 02:28:23 +0200

    added city page

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import CurrentWeather from './src/screens/CurrentWeather.js';
 import {View,StyleSheet} from 'react-native'
 import UpcomingWeather from './src/screens/UpcomingWeather.js';
-
+import City from './src/screens/City.js';
 const App = ()=>{
 return(
   <View style={styles.container}>
-    
+    <City/>
     <CurrentWeather/>
     <UpcomingWeather/>
   </View>
