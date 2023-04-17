c26bb8a5f050b70688f86b13d90dcc54db20f8e7
Le-Fox <smkupa@gmail.com>
2023-04-17 13:48:46 +0200

    refactoring app.js

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -3,64 +3,33 @@ import { ActivityIndicator, View ,StyleSheet} from 'react-native';
 import { NavigationContainer  } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Tabs from './src/components/Tabs.js'
-import * as Location from 'expo-location'
-import {API_KEY} from '@env'
+import { useGetWeather } from './src/hooks/useGetWeather.js';
+
 
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const Tab = createBottomTabNavigator()
 const App =()=>{
-  const [loading,setLoading]= useState(true)
-  const [error,setError]=useState(null)
-  const [weather,setWeather]=useState([])
-  const [lat,setLat]= useState([])
-  const [lon,setLon]= useState([])
 
-  const fetchWeatherData = async () =>{
+  const [loading, error, weather] =useGetWeather()
 
-    try {
-      const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`)
-      const data = await res.json()
-      setWeather(data)
-    } catch (e) {
-      setError("Couldn't fetch weather data")
-    }finally {
-      setLoading(false)
-    }
 
+  if(weather&&weather.list){
+    return(
+      <NavigationContainer>
+        <Tabs weather={weather}/>
+      </NavigationContainer>
+    )
   }
-  useEffect(() =>{
-    (async () =>{
-      let {status} = await Location.requestForegroundPermissionsAsync()
-      console.log(status,'s')
-      if (status !=='granted'){
-        setError('Permission to access location was denied')
-        return
-      }
-      let location = await Location.getCurrentPositionAsync({})
-      setLat(location.coords.latitude)
-      setLon(location.coords.longitude)
-    
-      await fetchWeatherData()
-    })()
-  },[lat,lon])
 
-
-
-  if (loading){
     return (
       <View style={styles.container}>
         <ActivityIndicator size={'large'} color={'blue'}/>
       </View>
     )
-  }
 
 
-return(
-  <NavigationContainer>
-    <Tabs/>
-  </NavigationContainer>
-)}
+}
 
 
 const styles = StyleSheet.create({
--- WeatherApp/src/screens/CurrentWeather.js
+++ WeatherApp/src/screens/CurrentWeather.js
@@ -4,24 +4,31 @@ import {View,Text,SafeAreaView, StyleSheet} from 'react-native'
 import RowText from '../components/RowText';
 import { weatherType } from '../utilities/WeatherType';
 
-const CurrentWeather = ()=>{
-  const{wrapper,container,temp,feels,highlow,highlow_wrapper,message,description,bodyWrapper}= styles
+const CurrentWeather = ({weatherData})=>{
+  const{wrapper,container,tempStyles,feels,highlow,highlow_wrapper,message,description,bodyWrapper}= styles
+
+  const{
+    main:{temp,feels_like,temp_max,temp_min},
+    weather
+  } = weatherData
+
+  const weatherCondition = weather[0].main
 return(
-  <SafeAreaView style={wrapper}>
+<SafeAreaView style={[wrapper,{backgroundColor:weatherType[weatherCondition].backgroundColor}]}>
      <View style={container}>
-      <Feather name={weatherType["Thunderstorm"].icon} size={100} color="black"/>
-      <Text style={temp}>6</Text>
-      <Text style={feels}>Feels like 5</Text>
+      <Feather name={weatherType[weatherCondition].icon} size={100} color="white"/>
+      <Text style={tempStyles}>{temp}</Text>
+      <Text style={feels}>{`Feels like ${feels_like}`}</Text>
       
-      <RowText messageOne={'High: 8'} 
-      messageTwo={'Low: 6'} 
+      <RowText messageOne={`High: ${temp_max}`} 
+      messageTwo={`Low: ${temp_min}`} 
       containerStyles={highlow_wrapper} 
       messageOneStyles={highlow} 
       messageTwoStyles={highlow}/>
     </View>
 
-    <RowText messageOne={"It's Sunny"} 
-     messageTwo={weatherType['Thunderstorm'].message} 
+    <RowText messageOne={weather[0].description} 
+     messageTwo={weatherType[weatherCondition].message} 
      containerStyles={bodyWrapper} 
      messageOneStyles={description} 
      messageTwoStyles={message}/> 
@@ -34,11 +41,11 @@ const styles = StyleSheet.create({
     alignItems:'center',
     justifyContent:'center'
   },wrapper:{
-    flex:1,
-    paddingTop: Platform.OS === 'android' ? 25 : 0,
+    flex:1, 
+      paddingTop: Platform.OS === 'android' ? 25 : 0,
     backgroundColor:"pink",
   },
-  temp:{
+  tempStyles:{
     color:"black",
     fontSize:48,
   },
@@ -52,6 +59,7 @@ const styles = StyleSheet.create({
   },
   highlow_wrapper:{
     flexDirection:"row"
+
   },
   bodyWrapper:{
     justifyContent:"flex-end",
